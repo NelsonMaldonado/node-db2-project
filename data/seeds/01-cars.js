53b6994dbae6c07d1a3b6ffd95bd9707279948bf
Nelson Maldonado <play2nino@gmail.com>
2022-01-18 23:22:16 -0800

    wip

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,34 @@
 // STRETCH
+exports.seed = function (knex) {
+  return knex("cars")
+    .truncate()
+    .then(function () {
+      // Inserts seed entries
+      return knex("cars").insert([
+        {
+          vin: "ljdsfoi",
+          make: "Ford",
+          model: "Mustang",
+          mileage: "4325",
+          title: "new",
+          transmission: "manual",
+        },
+        {
+          vin: "adfadf",
+          make: "ford",
+          model: "F150",
+          mileage: "2343243",
+          title: "new",
+          transmission: "manual",
+        },
+        {
+          vin: "grapes",
+          make: "chevrolet",
+          model: "camaro",
+          mileage: "2434323",
+          title: "new",
+          transmission: "manual",
+        },
+      ])
+    })
+}
