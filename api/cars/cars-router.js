43a5ac22746047fecbdd2e443e6c20108346b8d0
Nelson Maldonado <play2nino@gmail.com>
2022-01-18 22:49:23 -0800

    wip

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,14 +1,24 @@
-const knex = require("knex")
-
-const db = require("../../data/db-config")
-
 // DO YOUR MAGIC
 const express = require("express")
 const Cars = require("../cars/cars-model")
 const router = express.Router()
 
+// router.get("/", (req, res) => {
+//   res.send("The / endpoint is working")
+// })
+
 router.get("/", (req, res) => {
-  res.send("The / endpoint is working")
+  Cars.getAll()
+    .then((car) => {
+      res.json(car)
+    })
+    .catch((err) => {
+      res.status(500).json({ message: "could not get cars" })
+    })
+})
+
+router.get("/:id", (req, res) => {
+  res.send("getting buy id working well")
 })
 
 module.exports = router
