775ce3dc8765d4d885885df7b9a9ad1a395825f5
Nelson Maldonado <play2nino@gmail.com>
2022-01-18 22:39:06 -0800

    made models, index,  server

--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,17 +1,17 @@
 exports.up = async function (knex) {
   // DO YOUR MAGIC
-  await knex.schema.createTable('cars', (table)=>{
+  await knex.schema.createTable("cars", (table) => {
     table.increments()
-    table.text('vin',128).unique().notNullable()
-    table.text('make',128).notNullable()
-    table.text('model',128).notNullable()
-    table.integer('mileage',128).notNullable()
-    table.text('title',128)
-    table.text('transmission',128)
+    table.text("vin", 128).unique().notNullable()
+    table.text("make", 128).notNullable()
+    table.text("model", 128).notNullable()
+    table.integer("mileage", 128).notNullable()
+    table.text("title", 128)
+    table.text("transmission", 128)
   })
-};
+}
 
 exports.down = async function (knex) {
   // DO YOUR MAGIC
-  await 
-};
+  await knex.schema.dropTableIfExists("fruits")
+}
