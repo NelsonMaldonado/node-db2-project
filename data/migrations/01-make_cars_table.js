d633e480cb25e0c37f10aadbe5c1c09bc60aa754
Nelson Maldonado <play2nino@gmail.com>
2022-01-18 22:20:13 -0800

    wip

--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,7 +1,17 @@
-exports.up = function (knex) {
+exports.up = async function (knex) {
   // DO YOUR MAGIC
+  await knex.schema.createTable('cars', (table)=>{
+    table.increments()
+    table.text('vin',128).unique().notNullable()
+    table.text('make',128).unique().notNullable()
+    table.text('model',128).unique().notNullable()
+    table.text('mileage',128).unique().notNullable()
+    table.text('title',128).unique().notNullable()
+    table.text('transmission',128).unique().notNullable()
+  })
 };
 
-exports.down = function (knex) {
+exports.down = async function (knex) {
   // DO YOUR MAGIC
+  await 
 };
