8b9a16870d84f67d197a7385e6629a6a525398ca
Nelson Maldonado <play2nino@gmail.com>
2022-01-18 22:08:16 -0800

    wip

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,1 +1,10 @@
 // DO YOUR MAGIC
+const express = require("express")
+const Cars = require("../cars/cars-model")
+const router = express.Router()
+
+router.get("/", (req, res) => {
+  res.send("The / endpoint is working")
+})
+
+module.exports = router
